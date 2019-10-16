0afcae22d063474642c47ba5be7409e61fd7df90
jmf4nier <jmf4nier@gmail.com>
2019-10-15 22:34:32 -0500

    setup login and signup components, redux, thunk, material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-
+import Login from "./components/login/Login"
+import SignUp from './components/signup/Signup';
+import Home from './components/Home'
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/signup">
+          <SignUp/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
+  </Router>
+    
+    //  <Login/>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home () {
+    return(
+        <ul>
+            <li><h1>home page</h1></li>
+        </ul>
+    )
+};
