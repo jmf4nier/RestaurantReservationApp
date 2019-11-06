7f0b5908338f19feddec1b75c57bef04ae655e83
jmf4nier <jmf4nier@gmail.com>
2019-11-06 15:19:47 -0600

    shaped homepage and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./navbar/NavBar";
 
-export default function Home () {
-    return(
-        <ul>
-            <li><h1>home page</h1></li>
-        </ul>
-    )
-};
+export default function Home() {
+    return (
+        <div style={styles.main}>
+            <NavBar/>
+            <div style={styles.top}>1</div>
+            <div style={styles.middleTop}>2</div>
+            <div style={styles.middleBottom}>3</div>
+            <div style={styles.bottom}>4</div>
+        </div>
+    );
+}
+const styles = {
+    main: {
+        display: 'flex',
+        height: '100vh',
+        flexDirection: 'column',
+        
+    },
+    header: {
+        fontFamily: 'Times New Roman',
+        fontSize: 50
+    },
+    top: {
+        flex: '60%',
+        backgroundColor: 'blue'
+    },
+    middleTop: {
+        flex: '12%',
+        backgroundColor: 'red'
+    },
+    middleBottom: {
+        flex: '12%',
+        backgroundColor: 'green'
+    },
+    bottom: {
+        flex: '12%',
+        backgroundColor: 'yellow'
+    }
+}
